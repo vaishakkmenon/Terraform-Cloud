91dec6a37fcfa88f3a50ab8116688f09f4f4cd90
Vaishak Menon <vaishakkmenon@gmail.com>
2022-06-15 15:18:02 -0400

    uncommented vpc

--- network.tf
+++ network.tf
@@ -1,12 +1,12 @@
 
-# module "vpc" {
-#   source                                 = "terraform-google-modules/network/google//modules/vpc"
-#   project_id                             = "vm-ceracore-testing"
-#   network_name                           = "ignite-vpc"
-#   routing_mode                           = "GLOBAL"
-#   shared_vpc_host                        = false
-#   delete_default_internet_gateway_routes = false
-# }
+module "vpc" {
+  source                                 = "terraform-google-modules/network/google//modules/vpc"
+  project_id                             = "vm-ceracore-testing"
+  network_name                           = "ignite-vpc"
+  routing_mode                           = "GLOBAL"
+  shared_vpc_host                        = false
+  delete_default_internet_gateway_routes = false
+}
 
 output "test"{
     value = "test_output"
